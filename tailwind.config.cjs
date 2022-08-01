5c1b29ddcc19c01cc205f0853428fa809dea797c
Alan <mmckalan@gmail.com>
2022-08-01 14:37:47 +0100

    First

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {},
+  plugins: [],
+};
